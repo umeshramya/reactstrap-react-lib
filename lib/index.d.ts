4dfec91a86ac3cc4c763ddd5a346ac3eb0233adb
umeshramya <umeshbilagi@gmail.com>
2021-04-13 11:57:31 +0530

    v1.0.13

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,5 +3,6 @@ import ButtonP from "./lib/ButtonP";
 import ModelP from "./lib/ModelP";
 import FormSubmit from "./lib/forms/FormSubmit";
 import FormDelete from "./lib/forms/FormDelete";
-export { AlertP, ButtonP, ModelP, FormDelete, FormSubmit };
+import Table from "./lib/tables/Table";
+export { AlertP, ButtonP, ModelP, FormDelete, FormSubmit, Table };
 //# sourceMappingURL=index.d.ts.map
