c7371a9d8f1e910f8ad5c3d4a8cf64feccec9fc5
umeshramya <umeshbilagi@gmail.com>
2021-04-15 13:44:54 +0530

    done

--- src/lib/forms/FormDelete.tsx
+++ src/lib/forms/FormDelete.tsx
@@ -6,50 +6,7 @@ import AlertP from "../AlertP"
 import ModelP from "../ModelP"
 import {propMaster} from "../Interfaces/interfaces"
 
-// interface Props{
-//     /**This API uri for deleteing Post request */
-//     curUri:string;
-//     /**id is value by which the record has to be deleted  */
-//     id:any
-//      /**
-//      * This function is call back on success from server HTTP response 
-//      * @res This on success response from server
-//      */
-//       onSuccess: (res:AxiosResponse, successCallBack?:(...arg: any)=>any)=>string
-//       /**
-//        * This is props as callback  function to passesed inside onSuccess function
-//        */
-//       successCallBack?:(...arg: any)=>any
-  
-  
-//       /**
-//        * This function is call back on error from server HTTP response 
-//        * @error error eecived from server
-//        */
-//       onError: (error:AxiosError, errorCallback?:(...arg:any)=>any)=>string
-//       /**
-//        * This is props as a callback  function to passesed inside onError function
-//        */
-//       errorCallback?:(...arg: any)=>any
-//       /**
-//      * This function is for validation before submitting  in the form
-//      * In case of failed validadtion return string which is not equal to ""
-//      * If validation did succeed then return ""
-//      */
-//     validation?:()=>string
-//     /**
-//      * THis is form submit method
-//      * value could be "GET" | "POST" | "PUT" | "DELETE"
-//      */
-//     method?:RequestMethods
 
-//     /**
-//      * AxiosRequestConfig optional config to be passed in the api call
-//      */
-//     AxiosRequestConfig?:AxiosRequestConfig
-
-
-// }
 
 interface Props extends propMaster{
     /**id is value by which the record has to be deleted  */
--- src/lib/forms/FormSubmit.tsx
+++ src/lib/forms/FormSubmit.tsx
@@ -6,52 +6,6 @@ import AlertP from "../AlertP"
 import ModelP from "../ModelP"
 import {propMaster} from "../Interfaces/interfaces"
 
-// interface Props{
-//     /** req.body for post request */
-//     curObj:axiosMethodObj;
-//     /**API rroute uri for post request */
-//     curUri:string;
-//     /**This is Form input elements. do not add Form elemet thise get rendered inside the form itself */
-//     Inputs:ReactFragment
-    
-//      /** pass function with reseting the values i.e. curObj and etc */
-//     reset:()=>void;
-
-//     /**
-//      * This function is call back on success from server HTTP response 
-//      * @res This on success response from server
-//      */
-//     onSuccess: (res:AxiosResponse, successCallBack?:(...arg: any)=>any)=>string
-//     /**
-//      * This is props as callback  function to passesed inside onSuccess function
-//      */
-//     successCallBack?:(...arg: any)=>any
-
-
-//     /**
-//      * This function is call back on error from server HTTP response 
-//      * @error error eecived from server
-//      */
-//     onError: (error:AxiosError, errorCallback?:(...arg:any)=>any)=>string
-//     /**
-//      * This is props as a callback  function to passesed inside onError function
-//      */
-//     errorCallback?:(...arg: any)=>any
-//     /**
-//      * This function is for validation before submitting  in the form
-//      * In case of failed validadtion return string which is not equal to ""
-//      * If validation did succeed then return ""
-//      */
-//     validation?:()=>string
-
-
-//     /**
-//      * AxiosRequestConfig optional config to be passed in the api call
-//      */
-//     AxiosRequestConfig?:AxiosRequestConfig
-    
-    
-// }
 
 
 interface Props extends propMaster{
