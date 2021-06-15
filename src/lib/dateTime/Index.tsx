5e71ec1fd1efdd1087b807589ae4b4e892e7ffd5
umeshramya <umeshbilagi@gmail.com>
2021-06-15 13:44:10 +0530

    done

--- src/lib/dateTime/Index.tsx
+++ src/lib/dateTime/Index.tsx
@@ -14,23 +14,24 @@ export default function Index(props: Props): ReactElement {
         let preDate = dateTime.substring(0,10);
         let newDate = dateTime.replace(preDate, date);
         setDateTime(newDate);
-        console.log(newDate)
 
     }
 
     const setTime = (e:any)=>{
+        
         let time = e.target.value;
-        let preTime = dateTime.substring(12, 16);
-        let newTime = dateTime.replace(preTime, time);
+        let preTime = dateTime.substring(11, 19);
+        console.log(preTime)
+        let newTime = dateTime.replace(preTime, `${time}`);
         setDateTime(newTime);
 
-        console.log(newTime)
+       
     }
     return (
 
         <>
             <Input type="date" value={dateTime.substring(0,10) } onChange={(e)=>setDate(e)}/>
-            <Input type = 'time' value={dateTime.substring(12, 16)} onChange={(e)=>setTime(e)}/>
+            <Input type = 'time' value={dateTime.substring(11,19)} onChange={(e)=>setTime(e)}/>
 
         </>
     )
