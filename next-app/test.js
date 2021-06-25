e207b2e316d161c0029500a73e61cada55f3f23e
umeshramya <umeshbilagi@gmail.com>
2021-06-25 13:33:37 +0530

    done

---
+++ next-app/test.js
@@ -0,0 +1,11 @@
+const  {Moment} = require("reactstrap-react-lib")
+
+
+const moment = new Moment();
+
+let  curdate = moment.convertToDataBaseString();
+
+
+console.log(curdate);
+console.log(moment.setSystemTimeZoneToUTC().convertToDataBaseString(), "system time zone")
+console.log(moment.setUtcToSystemTimeZone().convertToDataBaseString(), "back to utc")
