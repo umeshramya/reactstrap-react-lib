df4cf42943f69b072bc3f45b63c6842423430cad
Umesh Bilagi <umeshbilagi@gmail.com>
2021-08-03 19:44:12 +0530

    done

--- lib/lib/forms/FormDelete.d.ts
+++ lib/lib/forms/FormDelete.d.ts
@@ -1,6 +1,9 @@
 /// <reference types="react" />
-import { propMaster } from "../Interfaces/interfaces";
-declare type Props = Omit<propMaster, "reset">;
+import { propMaster, recpthaSetting } from "../Interfaces/interfaces";
+declare type propsMasterwithoutReset = Omit<propMaster, "reset">;
+interface Props extends propsMasterwithoutReset {
+    recpthaSetting: recpthaSetting;
+}
 declare function Delete(props: Props): JSX.Element;
 export default Delete;
 //# sourceMappingURL=FormDelete.d.ts.map
