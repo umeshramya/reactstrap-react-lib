847e29f0a48e24cd6387082d9ca56de63e91cb6c
unknown <umeshbilagi@gmail.com>
2023-03-04 19:27:55 +0530

    link new tab

--- src/lib/units/LinkP.tsx
+++ src/lib/units/LinkP.tsx
@@ -5,15 +5,17 @@ import { FaHandPointRight } from "react-icons/fa";
 interface TableCellLinkProps {
   value: any;
   link: string;
+  newTab?:boolean
 }
 
 export default function LinkP({
   value,
   link,
+  newTab
 }: TableCellLinkProps): ReactElement {
   return (
     <>
-      <Link href={link}>
+      <Link href={link} target={newTab ? "_blank" : undefined}>
         <span style={{ color: "blue", cursor: "pointer", margin: "1rem" }}>
           <FaHandPointRight />
           {` ${value}`}
