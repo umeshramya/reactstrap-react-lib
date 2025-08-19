c00f6a36ea193d33a55d215979423dffee8d8470
umeshramya <umeshbilagi@gmail.com>
2025-08-19 08:19:16 +0530

    working with recapche

--- src/lib/forms/FormSubmit.tsx
+++ src/lib/forms/FormSubmit.tsx
@@ -165,17 +165,23 @@ const FormSubmit = ({
               e.preventDefault();
               if (recpthaSetting) {
                 //@ts-ignore
-                let grecaptcha = window.grecaptcha.enterprise || window.grecaptcha;
-                grecaptcha.ready(function () {
-                  grecaptcha
-                    .execute(recpthaSetting.siteKey, {
-                      action: recpthaSetting.action,
-                    })
-                    .then(function (token: any) {
-                      setrecaptchaToken(token);
-                      modRef.current?.show();
-                    });
-                });
+                let grecaptcha = window.grecaptcha?.enterprise || window.grecaptcha;
+                grecaptcha.ready(async()=>{
+                  const token = await grecaptcha.execute(recpthaSetting.siteKey, {action : recpthaSetting.action})
+                  setrecaptchaToken(token)
+                  modRef.current?.show();
+
+                })
+                // grecaptcha.ready(function () {
+                //   grecaptcha
+                //     .execute(recpthaSetting.siteKey, {
+                //       action: recpthaSetting.action,
+                //     })
+                //     .then(function (token: any) {
+                //       setrecaptchaToken(token);
+                //       modRef.current?.show();
+                //     });
+                // });
               } else {
                 modRef.current?.show();
               }
